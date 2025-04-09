920990694334ecbe7b370cbd62fcf8d7a4ad8cb5
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-04-09 09:11:57 +0100

    ts source codes

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let message: string = "Hello, World";
+let flag: boolean = false;
+let n: number = 42;
+
+function size(s: string): number {
+    console.log(s.length);
+ return s.length;
+}
+
+console.log(message);
+console.log(size("I am coming home"));

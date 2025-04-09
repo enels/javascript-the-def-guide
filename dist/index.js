52f3d9dfc02e582ba22707b6c31c440dfc136958
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-04-09 09:08:29 +0100

    compiled ts code

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+let message = "Hello, World";
+let flag = false;
+let n = 42;
+function size(s) {
+    console.log(s.length);
+    return s.length;
+}
+console.log(message);
+console.log(size("I am coming home"));
+//# sourceMappingURL=index.js.map
